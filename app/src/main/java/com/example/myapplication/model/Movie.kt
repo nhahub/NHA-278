52e22d0b57d0d6c05ea987771d8bba4f48ec6a84
ShahdSherif24 <sherifshahd517@gmail.com>
2025-11-21 19:08:20 +0200

    added missing entity

--- app/src/main/java/com/example/myapplication/model/Movie.kt
+++ app/src/main/java/com/example/myapplication/model/Movie.kt
@@ -1,6 +1,11 @@
 package com.example.myapplication.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorite_movies")
 data class Movie(
+    @PrimaryKey
     val id: Int,
     val title: String,
     val overview: String,
